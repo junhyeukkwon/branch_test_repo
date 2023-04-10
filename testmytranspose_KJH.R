9202361162b17514b4fd0d9dcca8db264dfd6a48
jackjack <85923524+junhyeukkwon@users.noreply.github.com>
2023-04-10 21:38:41 +0900

    Update testmytranspose_KJH.R

--- testmytranspose_KJH.R
+++ testmytranspose_KJH.R
@@ -17,26 +17,3 @@ myvar2 <- matrix(NA, nrow=0, ncol=0)
 myvar2
 mytranspose(myvar2)
 all.equal.raw((myvar2), mytranspose(mytranspose(myvar2)), check.attributes = FALSE) #TRUE
-
-myvar3 <- matrix(c(1,2), nrow=1, ncol=2)
-myvar3
-mytranspose(myvar3)
-myvar3[1] == mytranspose(myvar3)[1] #TRUE
-myvar3[2] == mytranspose(myvar3)[2] #TRUE
-all.equal.raw((myvar3), mytranspose(mytranspose(myvar3))) #TRUE
-
-myvar4 <- matrix(c(1,2), nrow=2, ncol=1)
-myvar4
-mytranspose(myvar4)
-myvar4[1] == mytranspose(myvar4)[1] #TRUE
-myvar4[2] == mytranspose(myvar4)[2] #TRUE
-all.equal.raw((myvar4), mytranspose(mytranspose(myvar4))) #TRUE
-
-myvar5 <- c(1,2,NA,3)
-myvar5
-mytranspose(myvar5)
-myvar5[1] == mytranspose(myvar5)[1] #TRUE
-myvar5[2] == mytranspose(myvar5)[2] #TRUE
-myvar5[3] == mytranspose(myvar5)[3] #NA
-myvar5[4] == mytranspose(myvar5)[4] #TRUE
-all.equal.raw(as.numeric(myvar5), as.numeric(mytranspose(mytranspose(myvar5)))) #TRUE
